193499d80dd4c036133ddce5c0e621737969463a
yunirsip <yunirsip@gmail.com>
2018-05-02 15:35:01 -0300

    testando se a identificacao esta de boa

--- trabalho.cpp
+++ trabalho.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-#define PI 3.1415926535897932384
+#define PI 3.141592653589793238
 
 /*Tamanho janela*/
 int windowW = 800;                        //Tamanho da tela na Horizontal.
@@ -68,7 +68,7 @@ GLint Yposition = 370;
 
 void Draw() {
     glLineWidth(3.0f);
-    
+
     //cefalotorax
     glBegin(GL_POLYGON);
     //Inicia o desenho de um poligono.
@@ -77,7 +77,7 @@ void Draw() {
         glVertex2f(30.0*(float)cos(i)+Xctx , 30.0*(float)sin(i)+Yctx);    //Coordenadas do circulo de raio 20 pixels.
     }
     glEnd();                                                            //Finaliza o circulo.
-    
+
     //abdomen
     glBegin(GL_POLYGON);                                                //Inicia o desenho de um poligono.
     glColor3f(0.02,0.02,0.02);                                                    //Cor do poligono.
@@ -85,105 +85,105 @@ void Draw() {
         glVertex2f(50.0*(float)cos(i)+Xabd , 50.0*(float)sin(i)+Yabd);    //Coordenadas do circulo de raio 20 pixels.
     }
     glEnd();
-    
+
     //perna 1
     glBegin(GL_LINES);
     glColor3f(1,0,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X11, Y11);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(1,0,0.0);
     glVertex2f(X11, Y11);
     glVertex2f(X12, Y12);
     glEnd();
-    
+
     //perna 2
     glBegin(GL_LINES);
     glColor3f(0,1,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X21, Y21);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(0,1,0.0);
     glVertex2f(X21, Y21);
     glVertex2f(X22, Y22);
     glEnd();
-    
+
     //perna 3
     glBegin(GL_LINES);
     glColor3f(0,0,1.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X31, Y31);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(0,0,1.0);
     glVertex2f(X31, Y31);
     glVertex2f(X32, Y32);
     glEnd();
-    
+
     //perna 4
     glBegin(GL_LINES);
     glColor3f(1,1,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X41, Y41);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(1,1,0.0);
     glVertex2f(X41, Y41);
     glVertex2f(X42, Y42);
     glEnd();
-    
+
     //perna 1d
     glBegin(GL_LINES);
     glColor3f(1,0,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X11d, Y11);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(1,0,0.0);
     glVertex2f(X11d, Y11);
     glVertex2f(X12d, Y12);
     glEnd();
-    
+
     //perna 2d
     glBegin(GL_LINES);
     glColor3f(0,1,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X21d, Y21);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(0,1,0.0);
     glVertex2f(X21d, Y21);
     glVertex2f(X22d, Y22);
     glEnd();
-    
+
     //perna 3d
     glBegin(GL_LINES);
     glColor3f(0,0,1.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X31d, Y31);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(0,0,1.0);
     glVertex2f(X31d, Y31);
     glVertex2f(X32d, Y32);
     glEnd();
-    
+
     //perna 4d
     glBegin(GL_LINES);
     glColor3f(1,1,0.0);
     glVertex2f(Xctx, Yctx);
     glVertex2f(X41d, Y41);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glColor3f(1,1,0.0);
     glVertex2f(X41d, Y41);
@@ -194,39 +194,39 @@ void Draw() {
 /*Funcao responsavel por desenhar todas as fuguras na janela.*/
 void onDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);        //Limpa o buffer responsavel por desenhar
-    
+
     /*Quando se altera o tamanho da tela a imagem acompanha*/
     glMatrixMode(GL_PROJECTION);        //Projeta a imagem na tela
     glLoadIdentity();                    //Carrega a MatrixMode
     gluOrtho2D(0,windowW,0,windowH);    //Limita a area da tela no primeiro quadrante
-    
+
     /*Desenha Circulo*/
     glMatrixMode(GL_MODELVIEW);            //Projeta a imagem na tela
     glLoadIdentity();                    //Carrega MatrixMode.
-    
-    
+
+
     glPushMatrix();
     {
         Draw();
     }
     glPopMatrix();
-    
+
     glutSwapBuffers();                    //Troca o buffer
     glFlush();
-    
+
 }
 
 /*Funcao responsavel pela movimentacao das figuras*/
 void Movimenta() {
-    
+
     //Verifica se o circulo saiu da janela.
     /*if(Xcir >windowW+20 || Xcir < -20 || Ycir >windowH+20 || Ycir < -20){
      Xcir = 40;                        //Reseta a posicao de inicio.
      Ycir = 40;
      }*/
-    
+
     cout << Xctx << " - " << Xposition << " - " << Yctx << " - " << Yposition << endl;
-    
+
     if(abs(Xposition - Xctx) > 2) {
         Xctx += incXcir; //incrementa o centro do circulo em X.
         Xabd += incXcir;
@@ -247,7 +247,7 @@ void Movimenta() {
         X41d += incXcir;
         X42d += incXcir;
     }
-    
+
     if(abs((- Yposition + windowH) - (Yctx - 19)) > 2) {
         Yctx += incYcir; //incrementa o centro do circulo em Y.
         Yabd += incYcir;
@@ -260,21 +260,21 @@ void Movimenta() {
         Y41 += incYcir;
         Y42 += incYcir;
     }
-    
+
     glutPostRedisplay();                //Redesenha as figuras na janela.
 }
 
 /*Recebe comandos do mouse */
 void Mouse(GLint botao, GLint estado,  GLint x, GLint y) {
-    
+
     int velPadrao = 100;                                    //Velocidade padronizada, sem ela fica muito rapido.
-    
+
     //Verifica se houve click com o botao esquerdo do mouse.
     if(botao == GLUT_LEFT_BUTTON && estado == GLUT_DOWN) {
-        
+
         Xposition = x;
         Yposition = y;
-        
+
         incXcir = (x - Xctx)/velPadrao;                        //Incrementa X do circulo conforme click do mouse.
         incYcir = ((-y + windowH)-Yctx)/velPadrao;            //Incrementa Y do circulo conforme click do mouse.
     }
@@ -282,7 +282,7 @@ void Mouse(GLint botao, GLint estado,  GLint x, GLint y) {
 
 /*Responsavel por realizar a configuracao inicial da janela.*/
 void Inicializa(int argc, char **argv) {
-    
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB );                //Buffer e esquema de cores.
     glutInitWindowSize(windowW,windowH);                        //Tamanho da tela em pixels.
@@ -293,7 +293,7 @@ void Inicializa(int argc, char **argv) {
 
 /*Coleta dados de entrada e realiza a saida atraves do monitor.*/
 void InputeOutput() {
-    
+
     glutMouseFunc(Mouse);                //Funcao responsavel pelos controles do mouse.
     glutDisplayFunc(onDisplay);                //Funcao responsavel por redesenhar a tela.
     Movimenta();
@@ -301,11 +301,11 @@ void InputeOutput() {
 
 /*Gerencia a criacao de janela, chamada de funcoes de controle e o loop principal*/
 int main(int argc, char **argv) {
-    
+
     Inicializa(argc, argv);
     InputeOutput();
     glutIdleFunc(InputeOutput);
     glutMainLoop();                        //Loop Infinito
-    
+
     return 0;
 }
