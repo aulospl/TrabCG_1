e6c39b7098c3bffccb78d7a4193a278f35914c88
Aulos Plautius <aulospl@gmail.com>
2018-05-30 15:56:24 -0300

    pernas

--- trab2.cpp
+++ trab2.cpp
@@ -1,9 +1,54 @@
+//Choyoung Lim               6436060
+//Luiz Eduardo Dorici        4165850
+//Aulos Plautius M Marino    7986409
+
 #include <iostream>
 #include <GL/glut.h>
 #include <GL/glu.h>
 
 int width = 1200;
 int height = 1200;
+
+
+//cefalotorax
+GLfloat Xctx = 0;                        //posicao em X do centro da cabeça da aranha.
+GLfloat Yctx = 0;                        //posicao em Y do centro do cabeça da aranha.
+//abdomen
+GLfloat Xabd = 0;                        //posicao em X do centro da bunda da aranha.
+GLfloat Yabd = -70;                        //posicao em Y do centro da bunda da aranha.
+
+GLfloat X11 = Xctx + -50;
+GLfloat Y11 = Yctx + 50;
+GLfloat X12 = Xctx + -75;
+GLfloat Y12 = Yctx + 125;
+
+GLfloat X21 = Xctx + -60;
+GLfloat Y21 = Yctx + 23;
+GLfloat X22 = Xctx + -119;
+GLfloat Y22 = Yctx + 85;
+
+GLfloat X31 = Xctx + -60;
+GLfloat Y31 = Yctx + -18;
+GLfloat X32 = Xctx + -129;
+GLfloat Y32 = Yctx + -48;
+
+GLfloat X41 = Xctx + -50;
+GLfloat Y41 = Yctx + -40;
+GLfloat X42 = Xctx + -95;
+GLfloat Y42 = Yctx + -105;
+
+GLfloat X11d = Xctx - -50;
+GLfloat X12d = Xctx - -75;
+
+GLfloat X21d = Xctx - -60;
+GLfloat X22d = Xctx - -119;
+
+GLfloat X31d = Xctx - -60;
+GLfloat X32d = Xctx - -129;
+
+GLfloat X41d = Xctx - -50;
+GLfloat X42d = Xctx - -95;
+
 /**
  * @desc Desenha eixos de um sistema de coordenadas.
  * @param {float*} basePoint Ponto de origem de um sistema de coordenadas.
@@ -63,6 +108,9 @@ void displayCallback()
 {
 	/** Limpa a janela APENAS uma vez */
 	glClear(GL_COLOR_BUFFER_BIT);
+	
+	/* Espessura da linha-perna */
+	glLineWidth(3.0f);
 
 	glColor3f(1.0f, 0.0f, 0.0f);
 	/** Desenha a janela esquerda inferior */
@@ -76,6 +124,10 @@ void displayCallback()
 	glutSolidSphere(2.0, 30, 30);
 	glTranslatef(-3.5, -0.0, -0.0);
 	glutSolidSphere(1.75, 30, 30);
+	glBegin(GL_LINES);
+  		glVertex3f(0.0f, 0.0f, 0.0f);
+  		glVertex3f(5.0f, 5.0f, 5.0f);
+	glEnd();
 	
 	/** Desenha a janela direita inferior */
 	glViewport(width/2, 0, width/2, height/2);
@@ -85,10 +137,13 @@ void displayCallback()
 	glRotatef(-90.0, 1.0, 0.0, 0.0);
 	glTranslatef(3.5, 0.0, 0.0);
 	glutSolidSphere(2.0, 30, 30);
-	glTranslatef(-3.5, -0.0, -0.0);
-	glutSolidSphere(1.75, 30, 30);
-
-
+	g
+  		glVertex3f(5.0f, 5.0f, 5.0f);
+	glEnd();
+	glBegin(GL_LINES);
+		glVertex3f(5.0f, 5.0f, 5.0f);
+		glVertex3f(0.0f, 0.0f, -5.0f);
+	glEnd();
 	/* Desenha janela esquerda superior*/
 	glViewport(0, height/2, width/2, height/2);
 	glLoadIdentity();
@@ -100,6 +155,10 @@ void displayCallback()
 	glTranslatef(-3.5, -0.0, -0.0);
 	glutSolidSphere(1.75, 30, 30);
 
+	glBegin(GL_LINES);
+  		glVertex3f(0.0f, 0.0f, 0.0f);
+  		glVertex3f(5.0f, 5.0f, 5.0f);
+	glEnd();
 	/* Desenha janela direita superior */
 	glViewport(width/2, height/2, width/2, height/2);
 	glLoadIdentity();		
@@ -110,8 +169,11 @@ void displayCallback()
 	glTranslatef(3.5, 0.0, 0.0);
 	glutSolidSphere(2.0, 30, 30);
 	glTranslatef(-3.5, -0.0, -0.0);
-	glutSolidSphere(1.75, 30, 30);
-	
+	glutSolidSphere(1.75, 30, 30);	
+	glBegin(GL_LINES);
+  		glVertex3f(0.0f, 0.0f, 0.0f);
+  		glVertex3f(5.0f, 5.0f, 5.0f);
+	glEnd();
 /** Dispara os comandos APENAS uma vez */
 	glFlush();
 }
@@ -120,7 +182,7 @@ void displayCallback()
  * @desc Função de callback para reshape.
  * @param {int} w Nova largura da janela.
  * @param {int} h Nova altura da janela.
- */
+	 */
 void reshapeCallback(int w, int h)
 {
 	/** Atualiza os valores da janela */
